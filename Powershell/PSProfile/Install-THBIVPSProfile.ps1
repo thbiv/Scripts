0268d9a88ff3dcc175e0ffa084e41943a3c1d6b5
Thomas Barratt <thomas.barratt@gmail.com>
2021-02-27 23:41:19 -0500

    update PSProfile script

--- Powershell/PSProfile/Install-THBIVPSProfile.ps1
+++ Powershell/PSProfile/Install-THBIVPSProfile.ps1
@@ -1,15 +1,17 @@
+[CmdletBinding()]
+Param (
+	[switch]$Force
+)
+
 If ($IsWindows) {
 	$OutputPath = $Env:temp
 } Else {
 	$OutputPath = $Home
 }
 
-If ((Test-Path -Path $($Profile.CurrentUserAllHosts)) -eq $False) {
-	New-Item -Path $($Profile.CurrentUserAllHosts) -ItemType File -Force
-}
 $ProfileIncludePath = "$(Split-Path -Path $($Profile.CurrentUserAllHosts) -Parent)\ProfileInclude"
-If (Test-Path -Path $ProfileIncludePath) {
-    New-Item -Path $ProfileIncludePath -ItemType Directory -Force
+If (-not(Test-Path -Path $ProfileIncludePath)) {
+    New-Item -Path $ProfileIncludePath -ItemType Directory -Force | Out-Null
 }
 
 $PorfileParams = @{
@@ -19,11 +21,16 @@ $PorfileParams = @{
 	'UseBasicParsing' = $True
 }
 $Response = Invoke-RestMethod @PorfileParams
-$URL = $Response.assets.browser_download_url
-Invoke-WebRequest -Uri $URL -OutFile "$OutputPath\$($Response.assets.name)"
-Expand-Archive -Path "$OutputPath\$($Response.assets.name)" -DestinationPath "$OutputPath" -Force -ErrorAction 'Stop'
-Copy-Item -Path "$OutputPath\Profile.ps1" -Destination $($Profile.CurrentUserAllHosts) -ErrorAction 'Stop' -Force
-Write-Host "Installed Powershell-Profile: $($Response.name)"
+$InstalledProfileBuild = (((Get-Content $Profile.CurrentUserAllHosts)[2] -split ':')[1]).TrimStart()
+If (($InstalledProfileBuild -ne $($Response.tag_name)) -or $Force) {
+	$URL = $Response.assets.browser_download_url
+	Invoke-WebRequest -Uri $URL -OutFile "$OutputPath\$($Response.assets.name)"
+	Expand-Archive -Path "$OutputPath\$($Response.assets.name)" -DestinationPath "$OutputPath" -Force -ErrorAction 'Stop'
+	Move-Item -Path "$OutputPath\Profile.ps1" -Destination $($Profile.CurrentUserAllHosts) -ErrorAction 'Stop' -Force
+	Write-Host "[Powershell-Profile] Installed Latest Release: $($Response.name)"
+} Else {
+	Write-Host "[Powershell-Profile] Latest Release is already installed"
+}
 
 $PSRParams = @{
 	'Uri' = 'https://api.github.com/repos/thbiv/PSReadline-Profile/releases/latest'
@@ -32,8 +39,13 @@ $PSRParams = @{
 	'UseBasicParsing' = $True
 }
 $Response = Invoke-RestMethod @PSRParams
-$URL = $Response.assets.browser_download_url
-Invoke-WebRequest -Uri $URL -OutFile "$OutputPath\$($Response.assets.name)"
-Expand-Archive -Path "$OutputPath\$($Response.assets.name)" -DestinationPath "$OutputPath" -Force -ErrorAction 'Stop'
-Copy-Item -Path "$OutputPath\PSReadline-Profile.ps1" -Destination "$(Split-Path -Path $($Profile.CurrentUserAllHosts) -Parent)\ProfileInclude\PSReadline-Profile.ps1" -ErrorAction 'Stop' -Force
-Write-Host "Installed PSReadline-Profile: $($Response.name)"
+$InstalledPSRBuild = (((Get-Content "$ProfileIncludePath\PSReadline-Profile.ps1")[2] -split ':')[1]).TrimStart()
+If (($InstalledPSRBuild -ne $($Response.tag_name)) -or $Force) {
+	$URL = $Response.assets.browser_download_url
+	Invoke-WebRequest -Uri $URL -OutFile "$OutputPath\$($Response.assets.name)"
+	Expand-Archive -Path "$OutputPath\$($Response.assets.name)" -DestinationPath "$OutputPath" -Force -ErrorAction 'Stop'
+	Move-Item -Path "$OutputPath\PSReadline-Profile.ps1" -Destination "$(Split-Path -Path $($Profile.CurrentUserAllHosts) -Parent)\ProfileInclude\PSReadline-Profile.ps1" -ErrorAction 'Stop' -Force
+	Write-Host "[PSReadline-Profile] Installed Latest Release: $($Response.name)"
+} Else {
+	Write-Host "[PSReadline-Profile] Latest Release is already installed"
+}
